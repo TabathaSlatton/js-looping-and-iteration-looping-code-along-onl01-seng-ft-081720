9cb6491024bfd392ec7e22b4e446b67c31a200d9
Tabatha Slatton <valbatha@gmail.com>
2020-11-16 22:34:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = [];
+ 
+function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+ 
+  return messages;
+}
+ 
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--
+    }
+}
